1ee2f169cbc6bf562aca7c5f96765e4545d7c8ab
Wilder Leon <wleona@ripley.com>
2023-08-08 08:52:20 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+function App() {
+  return (
+    <nav className='navbar'>
+      <div className='icon-container'>
+        <div className='round-decorator' />
+        <p>ROCKSHOP</p>
+      </div>
+    </nav>
+  )
+}
+
+export default App
